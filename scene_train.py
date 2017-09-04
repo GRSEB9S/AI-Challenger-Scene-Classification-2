37dd3e9b8b8d10a7a7a93a0135d05a35a52e6ba9
zhuchaoyang <zack.zcy@gmail.com>
2017-09-04 18:43:15 +0800

    first commit

--- scene_train.py
+++ scene_train.py
@@ -3,6 +3,9 @@ from torch.utils.data import DataLoader
 from torchvision import transforms, utils
 import argparse
 import matplotlib.pyplot as plt
+import resnet152_places365_scratch
+import torch
+import torch.nn as nn
 
 def image_Show():
 
@@ -13,8 +16,6 @@ def image_Show():
         imgs_Batch,label_Batch = sample_Batched['image'], sample_Batched['label']
         grid = utils.make_grid(imgs_Batch) # make a grid of mini-batch images
         plt.imshow(grid)
-        plt.pause(3)
-
 
 if __name__ == "__main__" :
 
@@ -28,5 +29,15 @@ if __name__ == "__main__" :
         ]))
 
     loader = DataLoader(dataset,batch_size=4,shuffle=True,num_workers=4,drop_last=True)
+
+    # it's the converted pytorch resnet152 trained on Places365_Standard from scratch
+    resnet152_places365_scratch = resnet152_places365_scratch.resnet152_places365
+    resnet152_places365_scratch.load_state_dict(torch.load("/data/chaoyang/Places_challenge2017/resnet152_places365.pth"))
+
+    if torch.cuda.is_available():
+
+        torch.cuda.set_device(0)
+        resnet152_places365_scratch = resnet152_places365_scratch.cuda()
+
 
 
